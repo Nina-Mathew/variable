faa880bdec69b3535769febb03ea1519542f5a34
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-09-28 12:53:09 -0400

    added press button a, b, and a+b

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    counter += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(counter)
+})
+input.onButtonPressed(Button.B, function () {
+    counter += -1
+})
 // Created by: Nina Mathew
 // Created on: 9/28/20
 // This program uses variables..
+let counter = 0
+counter = 0
